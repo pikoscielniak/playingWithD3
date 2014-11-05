e323cd5e5d6b244da41511a81e94114318103459
piotrekbass <piotrek.koscielniak@gmail.com>
2014-11-05 18:42:41 +0100

    bar chart

---
+++ barChart.js
@@ -0,0 +1,57 @@
+var w = 300;
+var h = 100;
+var padding = 2;
+var dataset = [5, 10, 14, 20, 25,
+    11, 25, 22, 18, 7];
+var svg = d3.select("body").append("svg")
+    .attr('width', w)
+    .attr('height', h);
+
+function colerPikcer(v) {
+    if (v <= 20) {
+        return "#666666";
+    }
+    else if (v > 20) {
+        return "#ff0033";
+    }
+}
+
+svg.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr({
+        x: function (d, i) {
+            return i * (w / dataset.length)
+        },
+        y: function (d) {
+            return h - d * 4;
+        },
+        width: w / dataset.length - padding,
+        height: function (d) {
+            return d * 4;
+        },
+        fill: function (d) {
+            return colerPikcer(d); //"rgb(" + (d * 10) + ", 0,0)";
+        }
+    });
+
+svg.selectAll('text')
+    .data(dataset)
+    .enter()
+    .append('text')
+    .text(function (d) {
+        return d;
+    })
+    .attr({
+        'text-anchor': "middle",
+        x: function (d, i) {
+            return i * (w / dataset.length) + (w / dataset.length - padding) / 2;
+        },
+        y: function (d) {
+            return h - (d * 4) + 14;
+        },
+        "font-family": "sans-serif",
+        "font-size": 12,
+        "fill": "#ffffff"
+    });
